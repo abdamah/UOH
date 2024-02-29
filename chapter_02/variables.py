ab871062254d2a1efe2d636f72d1f92c493edd77
Cabdillaahi Maxamuud Ismaaciil <cismail@telesom.com>
2024-02-29 14:03:26 +0300

    Update variable.py file.

--- chapter_02/variables.py
+++ chapter_02/variables.py
@@ -1,4 +1,9 @@
 #Variable : container that holds the data.
-
+name = "Abdallah"
+age = 23
 price = 29.99
-print(price)
+is_active = True
+print(name) # Abdallah
+print(age) # 23
+print(price) # 29.99
+print(is_active) # True
