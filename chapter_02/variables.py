28eed248acf0ae9e6741b61d0af1d022c2fbb942
Abdallah Mahmoud <javabyabdallah@gmail.com>
2025-04-16 09:08:07 +0300

    Clean up

--- chapter_02/variables.py
+++
@@ -1,9 +0,0 @@
-#Variable : container that holds the data.
-name = "Abdallah"
-age = 23
-price = 29.99
-is_active = True
-print(name) # Abdallah
-print(age) # 23
-print(price) # 29.99
-print(is_active) # True
