5d720e11dc097a048b6423bd78d2be47e336a91d
abdamah <abdamoh1@gmail.com>
2026-02-25 08:33:51 +0300

    First program in python

---
+++ python/programs2026/app.py
@@ -0,0 +1,3 @@
+'''This is first program in Python. print is a function in Python that outputs the specified message to the screen. In this case, it will print "Hello, World!" to the terminal.'''
+
+print("Hello, World!")
