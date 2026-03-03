01fb928db30235aedcf6522a7a98ab5e7120a3b0
abdamah <abdamoh1@gmail.com>
2026-03-03 10:51:57 +0300

    Write first program in python

--- python/programs2026/app.py
+++ python/programs2026/app.py
@@ -1,3 +1,1 @@
-'''This is first program in Python. print is a function in Python that outputs the specified message to the screen. In this case, it will print "Hello, World!" to the terminal.'''
-
-print("Hello, World!")
+print("Hello World!!")
