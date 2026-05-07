61d6c02cc21154342e88dc69839ba95dad47bfba
abdamah <abdamoh1@gmail.com>
2026-05-07 09:10:42 +0300

    Update guess game

--- python/programs2026/statements.py
+++ python/programs2026/statements.py
@@ -269,16 +269,16 @@ Loop continues until correct guess.
 """
 
 secret_number = 7
-guess = 0
 
-while guess != secret_number:
-
-    guess = int(input("Guess the number (1-10): "))
-
+while True:
+    guess = int(input("Guess the number: "))
     if guess == secret_number:
-        print("Congratulations! You guessed it right!")
+        print("Congratulations! You guessed the number.")
+        break
+    elif guess < secret_number:
+        print("Too low. Try again.")
     else:
-        print("Wrong guess. Try again!")
+        print("Too high. Try again.")
 
 
 # ==========================================================
